b16c88bf041b6de8df3663300c6c9f86f81067de
Adam Berg <adam@xyzdigital.com>
2023-01-19 08:41:38 -0800

    Add package.json and update server message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
+  res.write('Hello World - deployed with engram!');
   res.end();
 }).listen(8080);
