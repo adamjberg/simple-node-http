a9fa5ec2c032eaff90a706a4e1805dccc70c8792
Adam Berg <adam@xyzdigital.com>
2023-01-19 11:08:54 -0800

    Add version number

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World - deployed with engram cloud!');
+  res.write('Hello World v0.0.1 - deployed with engram cloud!');
   res.end();
 }).listen(8080);
