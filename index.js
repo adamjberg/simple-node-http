da8568f08367aaddef96791929a69ecbbdcea9ca
Adam <adam@xyzdigital.com>
2023-07-13 10:45:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World v0.0.3 - deployed with eg deploy!');
+  res.write('Hello World v0.0.4 - deployed with eg deploy!');
   res.end();
 }).listen(process.env.PORT || 3000);
